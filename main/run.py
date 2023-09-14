8d9a5600cba6c2f262d1bb04ce6db8477ae48e50
kavi <kavipriya.kousalya2004@gmail.com>
2023-09-14 16:27:38 +0530

    frame rate and the duration of the video is change

--- main/run.py
+++ main/run.py
@@ -14,10 +14,10 @@ os.makedirs(output_folder, exist_ok=True)
 output_video_file = 'dehazed_video.mp4'
 
 webCam = cv2.VideoCapture(0)
-frame_rate = 30  # Frames per second
+frame_rate = 60  # Frames per second
 
 # Capture 5 frames and dehaze them
-for current_frame in range(5):
+for current_frame in range(15):
     success, frame = webCam.read()
     
     if not success:
@@ -31,37 +31,37 @@ for current_frame in range(5):
     # Dehaze the captured frame
     HazeCorrectedImg, haze_map = image_dehazer.remove_haze(frame, showHazeTransmissionMap=False)
     
+    # Save the dehazed frame
     output_path = os.path.join(output_folder, f'dehazed_{current_frame}.jpg')
     cv2.imwrite(output_path, HazeCorrectedImg)
-    
+
     if cv2.waitKey(1) & 0xFF == ord('w'):
         break
 
 webCam.release()
 cv2.destroyAllWindows()
 
-# Convert dehazed images to a video
+# Convert dehazed images to a video with extended frame duration
 dehazed_image_files = sorted([os.path.join(output_folder, img) for img in os.listdir(output_folder)])
 
 # Load the first image to get dimensions
 sample_image = cv2.imread(dehazed_image_files[0])
 height, width, layers = sample_image.shape
 
-# Define the codec and create a VideoWriter object
+# Define the codec and create a VideoWriter object with a longer frame duration
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # You can change the codec as needed
-out = cv2.VideoWriter(output_video_file, fourcc, 30.0, (width, height))
+frame_duration = 1  # Set the frame duration in seconds (e.g., 5 seconds)
+out = cv2.VideoWriter(output_video_file, fourcc, frame_rate / frame_duration, (width, height))
 
 # Loop through dehazed images and add them to the video
 for image_file in dehazed_image_files:
     frame = cv2.imread(image_file)
-    out.write(frame)
+    # Write the same frame multiple times to extend its duration
+    for _ in range(int(frame_rate * frame_duration)):
+        out.write(frame)
 
 # Release the VideoWriter and close all OpenCV windows
 out.release()
 cv2.destroyAllWindows()
 
 print(f'Dehazed video saved as {output_video_file}')
-
-
-
-
