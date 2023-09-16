01dfea4b239288b2ba305bc39d478b00da189ba9
Dream <138191954+rajiv2004-cloud@users.noreply.github.com>
2023-09-16 11:35:52 +0530

    added testrun for reducing lagging

---
+++ testmodel/testrun.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+# Import the image_dehazer class from your existing code
+from image_dehazer import image_dehazer
+
+# Initialize the dehazer
+dehazer = image_dehazer(airlightEstimation_windowSze=15, boundaryConstraint_windowSze=3, C0=20, C1=300,
+                        regularize_lambda=0.1, sigma=0.5, delta=0.85, showHazeTransmissionMap=False)
+
+# Open a connection to the webcam
+cap = cv2.VideoCapture(0)
+
+# Set the desired frame rate (e.g., 30 frames per second)
+desired_frame_rate = 15
+cap.set(cv2.CAP_PROP_FPS, desired_frame_rate)
+
+while True:
+    # Read a frame from the webcam
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    # Apply the dehazing model to the frame
+    dehazed_frame, _ = dehazer.remove_haze(frame)
+
+    # Display the dehazed frame
+    cv2.imshow('Dehazed Video', dehazed_frame)
+
+    # Press 'q' to exit the loop and close the window
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and close all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
